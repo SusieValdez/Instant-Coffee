a8e363f83a10925d21024bbab578be23b1821129
SusieHatter <susiivaldez@gmail.com>
2022-04-07 00:20:46 -0500

    add animated player and movement

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,13 +1,17 @@
 extends KinematicBody2D
 
-export (int) var speed = 100
-export (int) var jump_speed = -700
-export (int) var gravity = 35
-export (int) var max_fall_speed = 700
+export (int) var speed
+export (int) var jump_speed
+export (int) var gravity
+export (int) var hard_gravity
+export (int) var max_fall_speed
 
 var velocity = Vector2.ZERO
 
-func _process(delta):
+onready var animationPlayer = $AnimationPlayer
+onready var sprite = $Sprite
+
+func _process(_delta):
 	if Input.is_action_just_pressed("menu"):
 		get_tree().quit()
 
@@ -15,10 +19,32 @@ func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
 		velocity.x = speed
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		velocity.x = -speed
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-			velocity.y = jump_speed
-	velocity.y += gravity
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y += jump_speed
+	
+	# If released jump before max jump height, start falling
+	if velocity.y < 0 and not is_on_floor() and Input.is_action_just_released("jump"):
+		velocity.y = 0
+	# If falling to ground, or just released jump, fall faster
+	if velocity.y > 0 or (not is_on_floor() and Input.is_action_just_released("jump")):
+		velocity.y += hard_gravity * delta
+	else:
+		velocity.y += gravity * delta
+
 	velocity.y = clamp(velocity.y, jump_speed, max_fall_speed)
+
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
+	if is_on_floor():
+		if velocity.x == 0:
+			animationPlayer.play("Idle")
+		else:
+			animationPlayer.play("Run")
+	else:
+		animationPlayer.play("Jump")
+
 	velocity = move_and_slide(velocity, Vector2.UP)
