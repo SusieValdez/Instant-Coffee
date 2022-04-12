f6709457933f29e69ef248e1c078af6e78efc30d
SusieHatter <susiivaldez@gmail.com>
2022-04-11 19:00:42 -0500

    update level 1

--- entities/Ben/Ben.gd
+++ entities/Ben/Ben.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
 onready var animationPlayer = $AnimationPlayer
+onready var level = get_node("../..")
 
 var orders = []
 
 func _ready():
 	animationPlayer.play("Idle")
-	orders = Globals.orders_by_level[get_node("..").name]
+	orders = Globals.orders_by_level[level.name]
 
 func _on_Area2D_body_entered(body):
 	if body is Player or body is BUTLER:
